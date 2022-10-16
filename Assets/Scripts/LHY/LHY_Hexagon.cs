a8f3dd536b340b4d36bfc0af6b62fc87531abe13
leeHY22 <99051809+leeHY22@users.noreply.github.com>
2022-10-17 01:46:58 +0900

    ,

---
+++ Assets/Scripts/LHY/LHY_Hexagon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LHY_Hexagon : MonoBehaviour
+{
+    public GameObject hexFactory;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
