980c37047ef40b5018c3036b59de640a34f5ee83
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-10-17 16:16:53 +0900

    .

---
+++ Assets/SJH/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Select();
+    }
+
+    public void Select()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            if (Input.GetButton("Fire1"))
+            {
+
+
+                if (hitInfo.transform.tag == "Furniture")
+                {
+                    GameObject furniture = hitInfo.transform.gameObject;
+                    name = furniture.name;
+                    print(furniture.name);
+
+                }
+            }
+        }
+    }
+}
