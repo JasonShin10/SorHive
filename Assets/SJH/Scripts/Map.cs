c237f2220726400220b60a781b1fb936f58fd722
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-10-19 17:34:54 +0900

    .

--- Assets/SJH/Scripts/Map.cs
+++ Assets/SJH/Scripts/Map.cs
@@ -7,20 +7,31 @@ public class Map : MonoBehaviour
     public GameObject quadFactory;
     public int tileX = 5;
     public int tileZ = 5;
-
+    public GameObject cube;
     GameObject currCube;
     GameObject floor;
     void Start()
     {
         for(int i = 0; i <= tileX; i++)
         {
+            for(int j =0; j <= tileX; j++)
+            {
             floor = Instantiate(quadFactory);
-            Vector3 firstPos = floor.transform.position;
-            firstPos.x += i;
+            Vector3 firstPos = transform.position;
+            firstPos.x += j;
+            firstPos.z += i;               
             floor.transform.position = firstPos;
- 
+                floor.transform.rotation = transform.rotation;
+            }
         }
 
+        //for(int i = 0; i <= tileZ; i++)
+        //{
+        //    floor = Instantiate(quadFactory);
+        //    Vector3 firstPos = floor.transform.position;
+        //    firstPos.z += i;
+        //    floor.transform.position = firstPos;
+        //}
     }
 
     void Update()
@@ -31,6 +42,7 @@ public class Map : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
+                //currCube = Instantiate(cube);
                 currCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 int x = (int)(hit.point.x);
                 int z = (int)(hit.point.z);
