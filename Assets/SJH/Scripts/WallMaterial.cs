2faa22ce6fca183824762df140ab36b794ad6391
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-10-22 21:46:01 +0900

    .

--- Assets/SJH/Scripts/WallMaterial.cs
+++ Assets/SJH/Scripts/WallMaterial.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class WallMaterial : MonoBehaviour
 {
     public Material[] mats;
+    MeshRenderer rb;
+
     // Start is called before the first frame update
     void Start()
     {
         mats = Resources.LoadAll<Material>("WallPaper");
-        Renderer rd = GetComponent<MeshRenderer>();
-        Material mat = rd.material;
-
+        rb = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKey("space"))
+        {
+            rb.material = mats[0];
+        }
     }
 }
