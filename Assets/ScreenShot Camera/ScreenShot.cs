b529091582416dd0ffc7b284285b9d32c690a4a3
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-10-30 23:06:59 +0900

    .

--- Assets/ScreenShot Camera/ScreenShot.cs
+++ Assets/ScreenShot Camera/ScreenShot.cs
@@ -50,7 +50,8 @@ namespace CustomUtils
             texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
             texture.Apply();
 
-            File.WriteAllBytes($"{Application.dataPath}/{screenShotName}.png", texture.EncodeToPNG());
+            File.WriteAllBytes($"{Application.dataPath + "/Resources/ZRoomImage"} /{screenShotName}.png", texture.EncodeToPNG());
+
         }
 
         private void Start()
