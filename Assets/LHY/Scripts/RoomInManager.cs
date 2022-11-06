23afc662452ca47595703c34c1629b4f8a7143a4
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-11-06 13:58:24 +0900

    .

--- Assets/LHY/Scripts/RoomInManager.cs
+++ Assets/LHY/Scripts/RoomInManager.cs
@@ -14,7 +14,7 @@ public class RoomInManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateFeedUI();
     }
 
     // Update is called once per frame
